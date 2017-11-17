217e231836ffefa8c427840d57d8543938ff2370
anatoliiibrahimov <anatolii.ibrahimov@gmail.com>
2017-11-17 11:19:50 +0200

    Added error handling

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,36 +1,46 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import { error } from 'util';
+import {beginAjaxCall,ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
-  return { type: types.LOAD_COURSES_SUCCESS, courses };
+  return {
+    type : types.LOAD_COURSES_SUCCESS,courses
+  };
 }
 
 export function updateCourseSuccess(course) {
-  return { type : types.UPDATE_COURSE_SUCCESS,course };
+  return {
+    type : types.UPDATE_COURSE_SUCCESS,course
+  };
 }
 
 export function createCourseSuccess(course) {
-  return { type : types.CREATE_COURSE_SUCCESS,course };
+  return {
+    type : types.CREATE_COURSE_SUCCESS,course
+  };
 }
 
 export function loadCourses() {
   return function (dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
+      dispatch(ajaxCallError());
       throw(error);
     });
   };
 }
 
 export function saveCourse(course) {
-  return function (dispatch, getState) {
-    return courseApi.saveCourse(course).then(course => {
+  return function (dispatch,getState) {
+    dispatch(beginAjaxCall());
+    return courseApi.saveCourse(course).then(savedCoutse => {
       course.id
-      ? dispatch(updateCourseSuccess(course))
-      : dispatch(createCourseSuccess(course));
+      ? dispatch(updateCourseSuccess(savedCoutse))
+      : dispatch(createCourseSuccess(savedCoutse));
     }).catch(error => {
+      dispatch(ajaxCallError());
       throw(error);
     });
   };
